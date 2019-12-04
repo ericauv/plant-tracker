5507316183b7520ad2d7f8308bc997d3d0322d88
ericauv <eriauv17@gmail.com>
2019-12-03 22:16:07 -0500

    add ApolloClient, styled-components, update tsconfig, add Card

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { render } from '@testing-library/react-native';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import Card from './components/Card';
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4001/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Test</Text>
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Test</Text>
+        <Card></Card>
+      </View>
+    </ApolloProvider>
   );
 };
 
