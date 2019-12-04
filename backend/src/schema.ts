5c8b2885474ade17aa53f9c7ac3a9ea998c49b8d
Shini Ko <ko.shmily@gmail.com>
2019-12-03 23:47:35 -0500

    Schema

---
+++ backend/src/schema.ts
@@ -0,0 +1,40 @@
+import { gql } from 'apollo-server';
+
+const schema = gql`
+  type User {
+    id: ID!
+    name: String
+    plants: [Plant]
+    climate: String
+  }
+
+  type Plant {
+    id: ID!
+    name: String
+    species: Species
+    photo: String
+    wateringInterval: Int
+    lastWatered: String
+    nextWatering: String
+    description: String
+  }
+
+  type Species {
+    id: ID!
+    species: String
+    soilType: String
+    waterAmount: String
+    lightAmount: String
+    temperature: [Int]
+    description: String
+  }
+
+  type Query {
+    user(id: ID!): User
+    plants(userId: ID): [Plant]
+    species(speciesName: String): [Species]
+    testMessage: String!
+  }
+`;
+
+export default schema;
