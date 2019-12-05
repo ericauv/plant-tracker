8212d51c74eca02fe5af94b152914302dc00e62e
ericauv <eriauv17@gmail.com>
2019-12-04 22:58:02 -0500

    add PlantCard, add NextWatering

---
+++ frontend/components/NextWatering.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components';
+import { differenceInCalendarDays } from 'date-fns';
+interface Props {
+  nextWatering: number;
+}
+
+interface TextProps {
+  day: string;
+  color: string;
+}
+
+const NextWatering = (props: Props) => {
+  const getTextProps = (daysToNextWatering: number): TextProps => {
+    let day;
+    let color;
+    // Water Today
+    if (daysToNextWatering === 0) {
+      day = 'Today';
+      color = '#CCCC00';
+      //   color = '#F0E68C';
+    }
+    // Water Overdue
+    else if (daysToNextWatering < 0) {
+      color = 'red';
+      if (daysToNextWatering === -1) {
+        day = 'Yesterday';
+      } else if (daysToNextWatering <= -2) {
+        day = `${Math.abs(daysToNextWatering)} days ago`;
+      }
+    }
+    // Water in Future
+    else if (daysToNextWatering > 0) {
+      color = 'green';
+      if (daysToNextWatering === 1) {
+        day = 'Tomorrow';
+      } else if (daysToNextWatering >= 2) {
+        day = `In ${daysToNextWatering} days`;
+      }
+    }
+    return { day, color };
+  };
+
+  const daysToNextWatering = differenceInCalendarDays(
+    new Date(props.nextWatering),
+    new Date()
+  );
+  console.log(daysToNextWatering);
+
+  const textProps = getTextProps(daysToNextWatering);
+
+  return (
+    <View>
+      <Text style={{ color: textProps.color }}>{textProps.day}</Text>
+    </View>
+  );
+};
+
+export default NextWatering;
---
+++ frontend/components/PlantCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import { View, Text } from 'react-native';
+import NextWatering from './NextWatering';
+
+interface PropsInterface {
+  name: string;
+  species: string;
+  nextWatering: number;
+}
+
+const PlantCardStyles = styled.View`
+  flex: 1;
+  background: whitesmoke;
+  margin: 5px;
+  border-radius: 5;
+  border: 1px solid black;
+  shadow-color: black;
+  shadow-offset: 0px 3px;
+  shadow-radius: 5px;
+  shadow-opacity: 0.8;
+  elevation: 2;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+const PlantCard = (props: PropsInterface) => {
+  return (
+    <PlantCardStyles>
+      <Text>{props.name}</Text>
+      <Text>{props.species}</Text>
+      <NextWatering nextWatering={props.nextWatering}></NextWatering>
+    </PlantCardStyles>
+  );
+};
+
+export default PlantCard;
