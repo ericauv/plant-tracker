2ec002b2de83d762239352ef60c3d068b79770c0
ericauv <eriauv17@gmail.com>
2019-12-13 13:12:40 -0500

    fix resolvers for new schema

--- backend/src/resolvers.ts
+++ backend/src/resolvers.ts
@@ -3,12 +3,13 @@ import { users } from './data/users';
 import { plants, Plant } from './data/plants';
 import { species, Species } from './data/species';
 
-let plantsWithSpecies = [...plants].map(plant => {
-  plant.species = {
-    ...species.find(species => species.id === plant.speciesId)
-  };
-  return plant;
-});
+const populatePlantsWithSpecies = (plantsArray: Plant[]) =>
+  [...plantsArray].map(plant => {
+    plant.species = {
+      ...species.find(species => species.id === plant.speciesId)
+    };
+    return plant;
+  });
 type ResolverFn = (parent: any, args: any, ctx: any, info: any) => any;
 interface ResolverMap {
   [field: string]: ResolverFn;
@@ -30,23 +31,25 @@ const Resolvers = {
       return species;
     },
     plants: (parent: any, args: { id: number }, ctx: any, info: any) => {
-      return plantsWithSpecies;
+      return populatePlantsWithSpecies(plants);
     },
     plantsByCategory: (parent: any, args: any, ctx: any, info: any) => {
       const daysToNextWatering = (nextWatering: Date): number =>
         differenceInCalendarDays(nextWatering, Date.now());
 
-      const overdue: Plant[] = plantsWithSpecies.filter(
-        (plant: Plant) => daysToNextWatering(plant.nextWatering) < 0
+      const overdue: Plant[] = populatePlantsWithSpecies(plants).filter(
+        (plant: Plant) =>
+          daysToNextWatering(plant.wateringInfo.nextWatering) < 0
       );
-      const today: Plant[] = plantsWithSpecies.filter(
-        (plant: Plant) => daysToNextWatering(plant.nextWatering) === 0
+      const today: Plant[] = populatePlantsWithSpecies(plants).filter(
+        (plant: Plant) =>
+          daysToNextWatering(plant.wateringInfo.nextWatering) === 0
       );
-      const future: Plant[] = plantsWithSpecies.filter(
-        (plant: Plant) => daysToNextWatering(plant.nextWatering) > 0
+      const future: Plant[] = populatePlantsWithSpecies(plants).filter(
+        (plant: Plant) =>
+          daysToNextWatering(plant.wateringInfo.nextWatering) > 0
       );
       const plantsCategorized = { nextWatering: { overdue, today, future } };
-      console.log(plantsCategorized);
 
       return plantsCategorized;
     }
@@ -74,13 +77,13 @@ const Resolvers = {
         },
         description: 'Fiddle leaf fig!'
       };
-      plantsWithSpecies.push(newPlant);
+      plants.push(newPlant);
       return newPlant;
     },
     waterPlant: (parent: any, args: { id: string }, ctx: any, info: any) => {
-      const plantToWater: Plant | undefined = plantsWithSpecies.find(
-        (plant: Plant) => plant.id === args.id
-      );
+      const plantToWater: Plant | undefined = populatePlantsWithSpecies(
+        plants
+      ).find((plant: Plant) => plant.id === args.id);
       if (plantToWater) {
         plantToWater.wateringInfo.lastWatered = new Date();
         plantToWater.wateringInfo.nextWatering = addDays(
